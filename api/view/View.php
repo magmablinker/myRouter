37dd976dac117969cba4d460ef481816630a4fc8
pmma_ <laurentSpam99@gmail.com>
2019-12-16 16:59:32 +0100

    removed unused code

--- api/view/View.php
+++ api/view/View.php
@@ -3,31 +3,6 @@
 abstract class View {
 
     /*
-     * Function that returns html text
-     * to the client that made the request.
-     * You have to override this function in it's
-     * children.
-     * 
-     * @param $text the text that will be rendered
-     */
-    
-    public static function render($text) {
-        print(sprintf(
-            '<!DOCTYPE html>
-            <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                    <title>Document</title>
-                </head>
-                <body>
-                    %s
-                </body>
-            </html>', $text));
-    }
-    
-    /*
      * Function that returns json text
      * to the client that made the request.
      * You have to override this function in it's
